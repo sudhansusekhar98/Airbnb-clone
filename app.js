187934c79ecdfd597da12a282a681198b02342ac
Sudhansu Sekhar <sudhansusekharpradhan766@gmail.com>
2025-05-18 19:51:36 +0530

    Add JS Files

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+// app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 
